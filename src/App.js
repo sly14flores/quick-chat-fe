f388104f0beb35ad80a2037d614e64d248d43af6
Sly Flores <sly@christian.com.ph>
2021-10-12 17:59:53 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { io } from "socket.io-client"
 
 function App() {
 
-  const socket = io.connect("https://quickchat-socket.herokuapp.com");  
+  const socket = io("https://quickchat-socket.herokuapp.com");  
 
   return (
     <Router>
